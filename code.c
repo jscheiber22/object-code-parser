d9d1b5cab8725880f631ebaec21a169d0f132455
James Scheiber <jscheiber22@gmail.com>
2023-09-23 15:59:26 -0700

    successfully completes the first row based on hard coded instruction, except for obj column

--- code.c
+++ code.c
@@ -5,8 +5,8 @@
 
 // Prototypes
 int opCodeText(int opCode, int lengthOfLists, int operations[][1]);
-int getTA(int bp, int x);
-int getAddrMode(int ni);
+int getTAAM(int bp);
+int getOAT(int ni);
 
 
 // Main function
@@ -17,16 +17,16 @@ int main(int argc, char *argv[]) {
 	//printf("%s", argv[1]); # prints filename of input file
 
 	int operations[][1] = {
-                0x18, 0x58, 0x90, 0x40, 0xB4, 0x28,
-                0x88, 0xA0, 0x24, 0x64, 0x9C, 0xC4,
-                0xC0, 0xF4, 0x3C, 0x30, 0x34, 0x38,
-                0x48, 0x00, 0x68, 0x50, 0x70, 0x08,
-                0x6C, 0x74, 0x04, 0xD0, 0x20, 0x60,
-                0x98, 0xC8, 0x44, 0xD8, 0xAC, 0x4C,
-                0xA4, 0xA8, 0xF0, 0xEC, 0x0C, 0x78,
-                0x54, 0x80, 0xD4, 0x14, 0x7C, 0xE8,
-                0x84, 0x10, 0x1C, 0x5C, 0x94, 0xB0,
-                0xE0, 0xF8, 0x2C, 0xB8, 0xDC
+		0x18, 0x58, 0x90, 0x40, 0xB4, 0x28,
+		0x88, 0xA0, 0x24, 0x64, 0x9C, 0xC4,
+		0xC0, 0xF4, 0x3C, 0x30, 0x34, 0x38,
+		0x48, 0x00, 0x68, 0x50, 0x70, 0x08,
+		0x6C, 0x74, 0x04, 0xD0, 0x20, 0x60,
+		0x98, 0xC8, 0x44, 0xD8, 0xAC, 0x4C,
+		0xA4, 0xA8, 0xF0, 0xEC, 0x0C, 0x78,
+		0x54, 0x80, 0xD4, 0x14, 0x7C, 0xE8,
+		0x84, 0x10, 0x1C, 0x5C, 0x94, 0xB0,
+		0xE0, 0xF8, 0x2C, 0xB8, 0xDC
         };
 
 
@@ -56,31 +56,29 @@ int main(int argc, char *argv[]) {
 		0,0,0,1,0
 	};
 
-	// immediate/simple	
-	char OAT[][15] = {
+	// pc/base/absolute (TA)
+	char TAAM[][16] = {
+		"pc", // 0x002000
+		"base", // 0x004000
+		"absolute", // ? direct? // bp == 0x000000 ?	// rubric says this is correct
+		"pc_indexed",
+		"base_indexed", 
+		"absolute_indexed" 
+	};
+
+	// immediate/simple	(how used)
+	char OAT[][9] = {
 		"immediate", // (ni == 0x010000)
+		"", // this space has to be here or it combines elements 0 and 1 :/
 		"simple", // else bc ni both = 0 or 1
 		"indirect" // (ni == 0x020000)
 	};
-
-	// pc/base/absolute
-	char TAAM[][15] = {
-		"pc", // 0x00200
-		"base", // 0x00400
-		"absolute" // ? direct? // bp == 0x000000 ?	// rubric says this is correct
-	};
-
-	//
-	// if simple -> check for index (x) otherwise don't 
-	//
-
-
 
 	char header[] = "INSTR FORMAT OAT TAAM OBJ";
 
 	int a = 0x032600;
 	int opCode = a & 0xFC0000;
-	opCode = 0x90;
+	//opCode = 0x90;
 
 	indexOfEverything = opCodeText(opCode, lengthOfLists, operations);
 
@@ -88,40 +86,75 @@ int main(int argc, char *argv[]) {
 	//printf("operat: %X\n", *operations[0]);
 	//printf("operat: %X\n", 0xB8);
 
-	int TA = getTA(a & 0x00600, a & 0x008000);
-	int addrMode = getAddrMode(a & 0x030000);
-
-
 	// spot for finding format, should divide length of data by 8, then value will == format
-	//if (isFormat2[indexOfEverything]) {
-	//	format = 2;	
-	//} else {
-	//	//          n, i     	    x             b,p 
-	//	getFormat(a & 0x030000, a & 0x008000, a & 0x006000); 
-	//}
+	if (*isFormat2[indexOfEverything]) {
+		printf("%d\n", *isFormat2[indexOfEverything]);
+		format = 2;	
+	} else {
+		if (a & 0x001000) { // just checks e
+			format = 4;
+		} else {
+			format = 3;
+		}
+	}
+
+
+	// OAT - immedaite/simple - how ta is used - addrmode, based on ni
+	int n = 0x020000;
+	int i = 0x010000;
+	int ni = 0x030000;
+	// TAAM - base/pc - ta itself, based on bp
+	int b = 0x004000;
+	int p = 0x002000;
+	int bp = 0x006000;
+	int x = 0x008000;
+	int e = 0x001000;
+
+	
+	int OATIndex = getOAT(a & ni);
+	//printf("TAAM: %s\n", OAT[0]);
+	int TAAMIndex = getTAAM(a & bp);
+	if ((OATIndex == 2) && ((a & x) == x)) { TAAMIndex += 3; } // if simple and x = 1, make it indexed
+
 
 	FILE *outFile;
-        outFile = fopen("obj_struct.txt", "w+");
+    outFile = fopen("obj_struct.txt", "w+");
 	fprintf(outFile, "%s\n", header);
 	//fprintf(outFile, "%X\n", opCodeText(opCode, lengthOfLists, operations));
-	fprintf(outFile, "%s %d\n", abbrev[indexOfEverything], format);
+	fprintf(outFile, "%s %d %s %s\n", abbrev[indexOfEverything], format, OAT[OATIndex], TAAM[TAAMIndex]);
 	fclose(outFile);
 
 	return 0;
 }
 
-int getTA(int bp, int x){
-	// if e=1 format = 3
-//	if (bp & 0x001000){
-//		return 3;
-//	} else if (
+// Gets target address value, returns index of correct char value in associated list
+int getTAAM(int bp){
+	int b = 0x004000;
+	int p = 0x002000;
+	if ((bp & p) == p) { // pc
+		return 0;
+	} else if ((bp & b) == b) { // base
+		return 1;
+	} else { // absolute
+		// technically both being 1 should fail, but if that issue arises it can be fixed in post :)
+		return 2;
+	}
 }
 
 
-int getAddrMode(int ni){
-//	if (ni 
+int getOAT(int ni){
+	int n = 0x020000;
+	int i = 0x010000;
+	if (((ni & i) == i) && ((ni & n) == n)) { // simple
+		return 2;
+	} else if ((ni & n) == n) { // indirect
+		return 3;
+	} else if ((ni & i) == i) { // immediate
+		return 0;
+	} else { // also simple (both n and i are 0)
+		return 2;
+	}
 }
-
 
 int opCodeText(int opCode, int lengthOfLists, int operations[][1]) {
 	
