e84b4252b08a58518cf16d3bdb28cf0fe3cecbf8
James Scheiber <jscheiber22@gmail.com>
2023-09-25 17:31:46 -0700

    now cleaned up version, moving to test with autograder...

--- code.c
+++ code.c
@@ -8,21 +8,82 @@
 #include <stdbool.h>
 
 // Prototypes
-int opCodeText(int opCode, int lengthOfLists, int operations[][1]);
+int opCodeIndex(int opCode, int lengthOfLists, int operations[][1]);
 int getTAAM(int bp);
 int getOAT(int ni, int format);
-int doTheThing(long a);
+int doTheThing(long instruction) ;
 
 
 // Main function
 int main(int argc, char *argv[]) {
 
+	// kill the program if a file is not provided
 	if (argc < 2){
+		printf("No filename provided in command execution. Exiting.");
 		return -1;
 	}
 
-	// printf("%s", argv[1]); # prints filename of input file
+	int lengthOfLists = 59;
+	int indexOfEverything, format, opCode;
 
+	// Lists of data to help assign values
+	int operations[][1] = {
+		0x18, 0x58, 0x90, 0x40, 0xB4, 0x28,
+		0x88, 0xA0, 0x24, 0x64, 0x9C, 0xC4,
+		0xC0, 0xF4, 0x3C, 0x30, 0x34, 0x38,
+		0x48, 0x00, 0x68, 0x50, 0x70, 0x08,
+		0x6C, 0x74, 0x04, 0xD0, 0x20, 0x60,
+		0x98, 0xC8, 0x44, 0xD8, 0xAC, 0x4C,
+		0xA4, 0xA8, 0xF0, 0xEC, 0x0C, 0x78,
+		0x54, 0x80, 0xD4, 0x14, 0x7C, 0xE8,
+		0x84, 0x10, 0x1C, 0x5C, 0x94, 0xB0,
+		0xE0, 0xF8, 0x2C, 0xB8, 0xDC
+    };
+	char abbrev[][6] = {
+		"ADD", "ADDF", "ADDR", "AND", "CLEAR", "COMP",
+		"COMPF", "COMPR", "DIV", "DIVF", "DIVR", "FIX",
+		"FLOAT", "HIO", "J", "JEQ", "JGT", "JLT",
+		"JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL",
+		"LDS", "LDT", "LDX", "LPS", "MUL", "MULF",
+		"MULR", "NORM", "OR", "RD", "RMO", "RSUB",
+		"SHIFTL", "SHIFTR", "SIO", "SSK", "STA", "STB",
+		"STCH", "STF", "STI", "STL","STS", "STSW",
+		"STT", "STX", "SUB", "SUBF", "SUBR", "SVC",
+		"TD", "TIO", "TIX", "TIXR", "WD"
+	};
+	int isFormat2[][1] = {
+		0,0,1,0,1,0,
+		0,1,0,0,1,0,
+		0,0,0,0,0,0,
+		0,0,0,0,0,0,
+		0,0,0,0,0,0,
+		1,0,0,0,1,0,
+		1,1,0,0,0,0,
+		0,0,0,0,0,0,
+		0,0,0,0,1,1,
+		0,0,0,1,0
+	};
+	// pc/base/absolute (TA)
+	char TAAM[][16] = {
+		"pc", // 0x002000
+		"base", // 0x004000
+		"absolute", // ? direct? // bp == 0x000000 ?	// rubric says this is correct
+		"pc_indexed",
+		"base_indexed", 
+		"absolute_indexed",
+		"    "
+	};
+	// immediate/simple	(how used)
+	char OAT[][9] = {
+		"immediate", // (ni == 0x010000)
+		"", // this space has to be here or it combines elements 0 and 1 :/
+		"simple", // else bc ni both = 0 or 1
+		"indirect", // (ni == 0x020000)
+		"    "
+	};
+
+
+	// add just the header info to the file
 	char header[] = "INSTR FORMAT OAT TAAM OBJ";
 	FILE *outFile;
     outFile = fopen("obj_struct.txt", "w+");
@@ -33,30 +94,79 @@ int main(int argc, char *argv[]) {
 	FILE *inFile;
 	char line[100];
 	inFile = fopen(argv[1], "r");
+	// iterate through given file line by line
 	while (fgets(line, sizeof(line), inFile)){
-		printf("%s", line);
+		// Only check text lines for instructions
 		if (line[0] == *"T"){
-			// int a = 0x032600;
-			// char* line = "T0000001C0500000320146910178D1BA0101BC0002F20073B2FF40F102EFD9041";
 			char *to = (char*) malloc(8);
 			strncpy(to, line+1, 6);
-			long startAddr = strtol(to, NULL, 16);
-			// startAddr >>= 8;
-			printf("str addr %X\n", startAddr);
+			long startAddr = strtol(to, NULL, 16); // starting address
 			strncpy(to, line+7, 2);
-			long objCodeLength = strtol(to, NULL, 16);
-			objCodeLength >>= 16;
-			// objCodeLength >>= 20;
-			printf("obj code len %X\n", objCodeLength);
+			long objCodeLength = strtol(to, NULL, 16); // length of line of object code
+			objCodeLength >>= 16; // bit shift to keep proper length
+
 			// objCodeLength * 2 bc for ex, 28bytes / 3 bytes per word * 6 bits per hex = 28/3*6 = 28*2 :)
 			for (int i = 9; i <= 9 + (objCodeLength * 2); i += 6) {
 				strncpy(to, line+i, 8);
-				long a = strtol(to, NULL, 16);
-				//a >>= 4;
-				printf("a %X\n", a);
-				i += doTheThing(a);
+				long instruction = strtol(to, NULL, 16);
+				// bitshift to make sure trailing values are correct
+				if (instruction <= 0xFFFF){
+					opCode = instruction & 0xFC00;
+					opCode >>= 8;
+				} else {
+					opCode = instruction & 0xFC000000;
+					opCode >>= 24;
+				}
+
+				// this variable is used to match the corresponding value within the above lists
+				indexOfEverything = opCodeIndex(opCode, lengthOfLists, operations);
+
+				// code for finding format based on given list and the value of e
+				if (*isFormat2[indexOfEverything]) {
+					format = 2;
+				} else {
+					if (instruction & 0x00100000) { // 0x00100000 = e
+						format = 4;
+					} else {
+						format = 3;
+						instruction >>= 8;
+					}
+				}
+
+				// defualts to blank space index of list for formatting
+				int OATIndex = 4;
+				int TAAMIndex = 6;
+
+				if (format == 3){
+					// OAT - immedaite/simple - how ta is used - addrmode, based on ni
+					int ni = 0x030000;
+					// TAAM - base/pc - ta itself, based on bp
+					int bp = 0x006000;
+					int x = 0x008000;
+				
+					OATIndex = getOAT(instruction & ni, format);
+					TAAMIndex = getTAAM(instruction & bp);
+					if ((OATIndex == 2) && ((instruction & x) == x)) { TAAMIndex += 3; } // if simple and x = 1, make it indexed
+				} else if (format == 4){
+					int ni = 0x03000000;
+					OATIndex = getOAT(instruction & ni, format);
+					TAAMIndex = 2; // always absolute for format 4
+				}
+
+
+				FILE *outFile;
+				outFile = fopen("obj_struct.txt", "a+");
+				fprintf(outFile, "%s    %d      %s    %s    %X\n", abbrev[indexOfEverything], format, OAT[OATIndex], TAAM[TAAMIndex], instruction) ;
+				fclose(outFile);
+
+				if (format == 4){
+					i += 2;
+				} else if (format == 3) {
+					i += 0;
+				}
 			}
 
+			// free allocated resources
 			free(to);
 		}
 	}
@@ -64,7 +174,8 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-// Gets target address value, returns index of correct char value in associated list
+
+// Gets TAAM value, returns index of correct char value in associated list
 int getTAAM(int bp){
 	int b = 0x004000;
 	int p = 0x002000;
@@ -78,23 +189,19 @@ int getTAAM(int bp){
 	}
 }
 
-
+// Gets OAT value, returns index of correct char value in associated list
 int getOAT(int ni, int format){
-	printf("%d\n", format);
 	int n, i, both;
+	// set variables for readability, different lengths for each format type
 	if (format == 3){
 		n = 0x020000;
 		i = 0x010000;
-		both = 0x030000;
+		both = 0x030000; // if both n and i are set to 1
 	} else {
 		n = 0x02000000;
 		i = 0x01000000;
 		both = 0x03000000;
 	}
-
-	// printf("ni %X\n", ni);
-	// printf("n %X\n", n);
-	// printf("i %X\n", i);
 	
 	if (ni == both) { // simple
 		return 2;
@@ -102,177 +209,20 @@ int getOAT(int ni, int format){
 		return 3;
 	} else if (ni == i) { // immediate
 		return 0;
+	} else { // also simple (both n and i are 0)
+		return 2;
 	}
-	// else { // also simple (both n and i are 0)
-	// 	return 2;
-	// }
 }
 
-int opCodeText(int opCode, int lengthOfLists, int operations[][1]) {
-	
+
+// Gets operation code value, returns index of correct char value in associated list
+int opCodeIndex(int opCode, int lengthOfLists, int operations[][1]) {
 	for (int i = 0; i < lengthOfLists - 1; i++) {
-//		printf("operat: %X\n", *operations[i]);
 		if (opCode == *operations[i]) {
-			// printf("opcode: %X\n", opCode);
-			// printf("operat: %X\n", *operations[i]);
-    			//return *operations[i];
 			return i;
 		}
   	}	
 
-	return 0x01; // 1 for fail
+	return 0x0; // 0 for fail
 
-}
-
-
-int doTheThing(long a){
-	int lengthOfLists = 59;
-	int indexOfEverything, format;
-	//printf("%s", argv[1]); # prints filename of input file
-
-	int operations[][1] = {
-		0x18, 0x58, 0x90, 0x40, 0xB4, 0x28,
-		0x88, 0xA0, 0x24, 0x64, 0x9C, 0xC4,
-		0xC0, 0xF4, 0x3C, 0x30, 0x34, 0x38,
-		0x48, 0x00, 0x68, 0x50, 0x70, 0x08,
-		0x6C, 0x74, 0x04, 0xD0, 0x20, 0x60,
-		0x98, 0xC8, 0x44, 0xD8, 0xAC, 0x4C,
-		0xA4, 0xA8, 0xF0, 0xEC, 0x0C, 0x78,
-		0x54, 0x80, 0xD4, 0x14, 0x7C, 0xE8,
-		0x84, 0x10, 0x1C, 0x5C, 0x94, 0xB0,
-		0xE0, 0xF8, 0x2C, 0xB8, 0xDC
-        };
-
-
-	char abbrev[][6] = {
-		"ADD", "ADDF", "ADDR", "AND", "CLEAR", "COMP",
-		"COMPF", "COMPR", "DIV", "DIVF", "DIVR", "FIX",
-		"FLOAT", "HIO", "J", "JEQ", "JGT", "JLT",
-		"JSUB", "LDA", "LDB", "LDCH", "LDF", "LDL",
-		"LDS", "LDT", "LDX", "LPS", "MUL", "MULF",
-		"MULR", "NORM", "OR", "RD", "RMO", "RSUB",
-		"SHIFTL", "SHIFTR", "SIO", "SSK", "STA", "STB",
-		"STCH", "STF", "STI", "STL","STS", "STSW",
-		"STT", "STX", "SUB", "SUBF", "SUBR", "SVC",
-		"TD", "TIO", "TIX", "TIXR", "WD"
-	};
-
-	int isFormat2[][1] = {
-		0,0,1,0,1,0,
-		0,1,0,0,1,0,
-		0,0,0,0,0,0,
-		0,0,0,0,0,0,
-		0,0,0,0,0,0,
-		1,0,0,0,1,0,
-		1,1,0,0,0,0,
-		0,0,0,0,0,0,
-		0,0,0,0,1,1,
-		0,0,0,1,0
-	};
-
-	// pc/base/absolute (TA)
-	char TAAM[][16] = {
-		"pc", // 0x002000
-		"base", // 0x004000
-		"absolute", // ? direct? // bp == 0x000000 ?	// rubric says this is correct
-		"pc_indexed",
-		"base_indexed", 
-		"absolute_indexed",
-		"    "
-	};
-
-	// immediate/simple	(how used)
-	char OAT[][9] = {
-		"immediate", // (ni == 0x010000)
-		"", // this space has to be here or it combines elements 0 and 1 :/
-		"simple", // else bc ni both = 0 or 1
-		"indirect", // (ni == 0x020000)
-		"    "
-	};
-
-	int opCode;
-
-	if (a <= 0xFFFF){
-		opCode = a & 0xFC00;
-		opCode >>= 8;
-	} else {
-		opCode = a & 0xFC000000;
-		opCode >>= 24;
-	}
-
-	indexOfEverything = opCodeText(opCode, lengthOfLists, operations);
-
-	printf("opcode: %X\n", opCode);
-
-	// spot for finding format
-	if (*isFormat2[indexOfEverything]) {
-		// printf("%d\n", *isFormat2[indexOfEverything]);
-		format = 2;
-	} else {
-		if (a & 0x00100000) { // just checks e
-			format = 4;
-		} else {
-			format = 3;
-			a >>= 8;
-		}
-	}
-
-	// printf("a for %X\n", a);
-
-	// int objValue = 0x0;
-	// if (format == 2){
-	// 	// objValue = a & 0x00FF0000;
-	// 	// objValue >>= 16;
-	// 	a >>= 24;
-	// } else if (format == 3){
-	// 	// objValue = a & 0x000FF000;
-	// 	// objValue >>= 12;
-	// 	a >>= 8;
-	// } else { // format 4
-	// 	// objValue = a & 0x000FFFFF;
-	// }
-
-	// defualts to blank spaces for formatting
-	int OATIndex = 4;
-	int TAAMIndex = 6;
-
-	if (format == 3){
-		// OAT - immedaite/simple - how ta is used - addrmode, based on ni
-		int n = 0x020000;
-		int i = 0x010000;
-		int ni = 0x030000;
-		// TAAM - base/pc - ta itself, based on bp
-		int b = 0x004000;
-		int p = 0x002000;
-		int bp = 0x006000;
-		int x = 0x008000;
-		int e = 0x001000;
-	
-		OATIndex = getOAT(a & ni, format);
-		//printf("TAAM: %s\n", OAT[0]);
-		TAAMIndex = getTAAM(a & bp);
-		if ((OATIndex == 2) && ((a & x) == x)) { TAAMIndex += 3; } // if simple and x = 1, make it indexed
-	} else if (format == 4){
-		int ni = 0x03000000;
-		OATIndex = getOAT(a & ni, format);
-		TAAMIndex = 2; // always absolute for format 4
-	}
-
-
-	FILE *outFile;
-    outFile = fopen("obj_struct.txt", "a+");
-	// fprintf(outFile, "%s\n", header);
-	// fprintf(outFile, "%X\n", opCodeText(opCode, lengthOfLists, operations));
-	// fprintf(outFile, "%s %d %s %s %X\n", abbrev[indexOfEverything], format, OAT[OATIndex], TAAM[TAAMIndex], objValue);
-	fprintf(outFile, "%s    %d      %s    %s    %X\n", abbrev[indexOfEverything], format, OAT[OATIndex], TAAM[TAAMIndex], a);
-	fclose(outFile);
-
-	if (format == 4){
-		return 2;
-	} else if (format == 3) {
-		return 0;
-	} 
-	// else {
-	// 	return -2;
-	// }
 }
